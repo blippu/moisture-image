a381cdab2baf6bdac633c07f48b4b7fb29049365
blippu <53516997+blippu@users.noreply.github.com>
2023-12-29 13:24:53 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+let index = 0
+let moisture = 0
+let myImage = images.createImage(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+    moisture = pins.analogReadPin(AnalogPin.P1)
+    moisture = moisture / 3000
+    moisture = moisture * 25
+    index = 0
+    for (let column = 4; column >=0 ; column--) {
+        for (let row = 0; row <= 4; row++) {
+            if (index <= moisture-1) {
+                index++
+                myImage.setPixel(row, column, true);
+            }
+        }
+    }
+myImage.showImage(0)
+    basic.pause(5000)
+})
